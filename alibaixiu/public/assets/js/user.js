7b1b7dd5eae11a0baff55234f1940e7788a5f2ce
ql1995 <848216307@qq.com>
2020-09-20 18:31:26 +0800

    用户删除、添加、修改模块

--- alibaixiu/public/assets/js/user.js
+++ alibaixiu/public/assets/js/user.js
@@ -1,94 +1,92 @@
-// 当表单发生提交行为的时候
-$('#userForm').on('submit', function () {
-	// 获取到用户在表单中输入的内容并将内容格式化成参数字符串
-	var formData = $(this).serialize();
-	// 向服务器端发送添加用户的请求
-	$.ajax({
-		type: 'post',
-		url: '/users',
-		data: formData,
-		success: function () {
-			// 刷新页面
-			location.reload();
-		},
-		error: function () {
-			alert('用户添加失败')
-		}
-	})
-	// 阻止表单的默认提交行为
-	return false;
+// 添加新用户
+$("#userForm").on('submit', function() {
+    var formData = $(this).serialize();
+    console.log(formData);
+    $.ajax({
+        type: 'post',
+        url: '/users',
+        data: formData,
+        success: function() {
+            location.reload();
+        },
+        error: function() {
+            alert('添加用户失败')
+        }
+    });
+    return false;
 });
-
-// 当用户选择文件的时候
-$('#modifyBox').on('change', '#avatar', function () {
-	// 用户选择到的文件
-	// this.files[0]
-	var formData = new FormData();
-	formData.append('avatar', this.files[0]);
-
-	$.ajax({
-		type: 'post',
-		url: '/upload',
-		data: formData,
-		// 告诉$.ajax方法不要解析请求参数
-		processData: false,
-		// 告诉$.ajax方法不要设置请求参数的类型
-		contentType: false,
-		success: function (response) {
-			console.log(response)
-			// 实现头像预览功能
-			$('#preview').attr('src', response[0].avatar);
-			$('#hiddenAvatar').val(response[0].avatar)
-		}
-	})
+// 事件委托的形式头像上传功能
+$("#modifyBox").on('change', "#avatar", function() {
+    var formData = new FormData();
+    // console.log(this.files[0]);
+    formData.append('avatar', this.files[0]);
+    $.ajax({
+        type: 'post',
+        url: '/upload',
+        data: formData,
+        // 让ajax方法不要解析请求参数
+        processData: false,
+        contentType: false,
+        success: function(res) {
+            //添加在预览上
+            $("#preview").attr('src', res[0].avatar);
+            // 添加在表单数据中
+            $("#hiddenAvatar").val(res[0].avatar);
+        }
+    });
 });
-
-// 向服务器端发送请求 索要用户列表数据
+// 用户列表展示功能
 $.ajax({
-	type: 'get',
-	url: '/users',
-	success: function (response) {
-		console.log(response)
-		// 使用模板引擎将数据和HTML字符串进行拼接
-		var html = template('userTpl', { data: response });
-		// 将拼接好的字符串显示在页面中
-		$('#userBox').html(html);
-	}
+    type: 'get',
+    url: '/users',
+    success: function(res) {
+        // console.log(res);
+        // 获取tbody
+        var html = template("userTpl", { data: res })
+        $('#userBox').html(html);
+    }
 });
-
-// 通过事件委托的方式为编辑按钮添加点击事件
-$('#userBox').on('click', '.edit', function () {
-	// 获取被点击用户的id值
-	var id = $(this).attr('data-id');
-	// 根据id获取用户的详细信息
-	$.ajax({
-		type: 'get',
-		url : '/users/' + id,
-		success: function (response) {
-			console.log(response)
-			var html = template('modifyTpl', response);
-			$('#modifyBox').html(html);
-		}
-	})
+// 事件委托编辑用户
+$("#userBox").on('click', '.edit', function() {
+    var id = $(this).attr('data-id');
+    $.ajax({
+        type: 'get',
+        url: '/users/' + id,
+        success: function(res) {
+            var html = template('modifyTpl', res);
+            // html方法覆盖之前的内容
+            $("#modifyBox").html(html);
+        }
+    });
 });
+$("#modifyBox").on('submit', '#modifyForm', function() {
 
-// 为修改表单添加表单提交事件
-$('#modifyBox').on('submit', '#modifyForm', function () {
-	// 获取用户在表单中输入的内容
-	var formData = $(this).serialize();
-	// 获取要修改的那个用户的id值
-	var id = $(this).attr('data-id');
-	// 发送请求 修改用户信息
-	$.ajax({
-		type: 'put',
-		url: '/users/' + id,
-		data: formData,
-		success: function (response) {
-			// 修改用户信息成功 重新加载页面
-			location.reload()
-		}
-	})
-
-	// 阻止表单默认提交
-	return false;
+    var formdata = $(this).serialize();
+    // console.log(formdata);
+    var id = $(this).attr('data-id');
+    $.ajax({
+        type: 'put',
+        url: '/users/' + id,
+        data: formdata,
+        success: function(res) {
+            location.reload();
+        }
+    });
+    return false;
 });
+// 删除功能
+$('#userBox').on('click', '.delete', function() {
+    var id = $(this).attr('data-id');
+    // console.log(id);
+    // 发送请求删除该项 需要事先确认一下
+    if (confirm("先生您确定要删除吗")) {
+        $.ajax({
+            type: 'delete',
+            url: '/users/' + id,
+            success: function() {
+                location.reload();
+            }
+        });
+    }
+
+})
